5bf32cb26efb360642eac8927b7456b43119e80d
emir-ltst <emkoshjumaev@gmail.com>
2026-09-12 14:31:10 +0300

    add function

--- src/components/Claculate.jsx
+++ src/components/Claculate.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import useState from 'react'
 
 const Claculate = () => {
+    function Calculutor() {
+    const [result, setResult] = useState("")
+
+    function handclick(value){
+        setRESULT(result + value)
+    }
+    }
+
+
+    const calculate = () =>{
+        try {
+            setResult(eval(result.toString()))
+        } catch (err) {
+            setRezult(err)
+        }
+    }
   return (
-    <div>Claculate</div>
+    <div>
+
+    </div>
   )
 }
 

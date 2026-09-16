36217ba243d03dbc917b651febb9cce80f47db3b
emir-ltst <emkoshjumaev@gmail.com>
2026-09-16 16:41:53 +0300

    hmm

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,11 +45,6 @@ export default function Home({ onAddToCart, onToggleFavorite, favorites }) {
                         Enter the archive <ArrowRight size={16} />
                       </Link>
                     </div>
-                                <div className="hero-rating-pill">
-              <span className="stars-sm">★★★★★</span>
-              <span>4.9 / 2,000+ users</span>
-            </div>
-          </div>
             </div>
         </section>
 

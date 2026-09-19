79b50e0c5b2bc8ff116fd01e096d23d777d501a8
emir-ltst <emkoshjumaev@gmail.com>
2026-09-19 17:20:10 +0300

    import products

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ArrowLeft, Heart, Plus, ShieldCheck, Star, Truck } from 'lucide-react'
 import { Link, useParams } from 'react-router-dom'
+import { products } from '../data/products'
 
 
 const ProductDetails = () => {

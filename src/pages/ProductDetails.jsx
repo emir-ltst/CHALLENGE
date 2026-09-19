0fbb5668ca7b232b28981a84eb1cbdd1fa8a1f41
emir-ltst <emkoshjumaev@gmail.com>
2026-09-19 17:19:30 +0300

    import link and useparams

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ArrowLeft, Heart, Plus, ShieldCheck, Star, Truck } from 'lucide-react'
+import { Link, useParams } from 'react-router-dom'
 
 
 const ProductDetails = () => {

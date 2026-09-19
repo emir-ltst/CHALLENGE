45c19399d1d2c4423c5259f0df2ebf3b442f549a
emir-ltst <emkoshjumaev@gmail.com>
2026-09-19 18:21:24 +0300

    add

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,2 +1,5 @@
 export default function ProductCard({ product, onAddToCart, onToggleFavorite, isFavorite, variant = 'default' }) {
+  if (variant === 'large') return <LargeCard product={product} onAddToCart={onAddToCart} onToggleFavorite={onToggleFavorite} isFavorite={isFavorite} />
+
+
 }

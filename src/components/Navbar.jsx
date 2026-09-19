bc3cf9414265f329121fc3aa5b87608fd30ca84b
emir-ltst <emkoshjumaev@gmail.com>
2026-09-19 17:44:10 +0300

    add secomd imooort

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Heart, Menu, Search, ShoppingBag, UserRound, X, Zap } from 'lucide-react'
+import { Link, NavLink } from 'react-router-dom'
 
 
 export default function Navbar({ cartCount, favoriteCount }) {

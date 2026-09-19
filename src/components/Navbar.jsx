68899f66a67fb5868d71ec542f5f59f6db8bfdf8
emir-ltst <emkoshjumaev@gmail.com>
2026-09-19 17:44:40 +0300

    add third import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Heart, Menu, Search, ShoppingBag, UserRound, X, Zap } from 'lucide-react'
 import { Link, NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 
 export default function Navbar({ cartCount, favoriteCount }) {
